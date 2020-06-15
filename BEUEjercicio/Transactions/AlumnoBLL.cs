39922677aab94f54c6a9e6a89205eec8da1a7550
MauroVRuizM <59812652+MauroVRuizM@users.noreply.github.com>
2020-06-15 14:52:27 -0500

    V1
    Version 1

---
+++ BEUEjercicio/Transactions/AlumnoBLL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUEjercicio.Transactions
+{
+    public class AlumnoBLL
+    {
+        public static Alumno Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.Alumnoes.Find(id);
+        }
+
+        public static void Create(Alumno a)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Alumnoes.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Update(Alumno alumno)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Alumnoes.Attach(alumno);
+                        db.Entry(alumno).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Alumno alumno = db.Alumnoes.Find(id);
+                        db.Entry(alumno).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<Alumno> List()
+        {
+            Entities db = new Entities();
+            return db.Alumnoes.ToList();
+        }
+    }
+}
