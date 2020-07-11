5e6e0a00dd45d91dffbad356f4f6b52a013dffe5
MauroVRuizM <59812652+MauroVRuizM@users.noreply.github.com>
2020-07-10 22:45:55 -0500

    WebApi Completada

--- WebAPI/Controllers/AlumnosController.cs
+++ WebAPI/Controllers/AlumnosController.cs
@@ -6,10 +6,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class AlumnosController : ApiController
     {
         [ResponseType(typeof(Alumno))]
@@ -17,6 +20,26 @@ namespace WebAPI.Controllers
         {
             List<Alumno> todos = AlumnoBLL.List();
             return Json(todos);
+        }
+
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult Delete(int id)
+        {
+            Message result = new Message();
+            try
+            {
+                AlumnoBLL.Delete(id);
+                result.title = "¡Correcto!";
+                result.text = "El alumno fue eliminado correctamente";
+                result.icon = "success";
+            }
+            catch (Exception ex) {
+                result.title = "¡Error!";
+                result.text = "Hubo un error al intentar eliminar ";
+                result.console = ex.Message;
+                result.icon = "error";
+            }            
+            return Json(result);
         }
     }
 }
---
+++ WebAPI/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Message
+    {
+        public string title { get; set; }
+        public string text { get; set; }
+        public string icon { get; set; }
+
+        public string console { get; set; }
+    }
+}
